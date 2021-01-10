a389a47b5a68ba3f15b55d67e97932fcf88ae2d8
MrCradily2 <s10204478@connect.np.edu.sg>
2021-01-10 12:52:22 +0800

    web pages and js script for pokemon by name

--- fetchByNature.js
+++ fetchByNature.js
@@ -14,11 +14,24 @@ function fetchByName(pokemonName) {
         const html = data.map(pokemon=>{
             return`
 
-                <div class="row">
-                            <div class="user">
-                                <p>${pokemon.name}</p>
-                            </div>
-                        </div>`;
+            <div class="card my-3" style="max-width: 960px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="..." class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Pokemon Search</h5>
+                  <p class="card-text">Name: ${pokemon.name}</p>
+                  <p class="card-text">Type: ${pokemon.types}</p>
+                  <p class="card-text">Height: ${pokemon.height}</p>
+                  <p class="card-text">Weight: ${pokemon.weight}</p>
+                  <p class="card-text">Abilities: ${pokemon.ability}</p>
+                </div>
+              </div>
+            </div>
+          </div>`
+        
         })
         .join("");
         console.log(html);
